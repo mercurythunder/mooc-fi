602b1bdf1c83ec9775a6a7dfac7a672dc8a8cb1c
mercurythunder <213652558+mercurythunder@users.noreply.github.com>
2025-06-05 22:46:28 +0200

    Finished Fix the code: Product

---
+++ part-1/part-1-4/03-fix-the-code-product.py
@@ -0,0 +1,7 @@
+number = int(input("Please type in the first number: "))
+number *= int(input("Please type in the second number: "))
+number *= int(input("Please type in the third number: "))
+
+product = number
+
+print("The product is", product)
