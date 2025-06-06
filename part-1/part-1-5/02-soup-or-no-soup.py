069105d01d2bcd1207304fae771ae317b56b99c0
mercurythunder <213652558+mercurythunder@users.noreply.github.com>
2025-06-06 19:36:59 +0200

    Finished Soup or no soup

---
+++ part-1/part-1-5/02-soup-or-no-soup.py
@@ -0,0 +1,6 @@
+name = input("Please tell me your name: ")
+if name != "Jerry":
+	soup = int(input("How many portions of soup? "))
+	per_soup = 5.9
+	print(f"The total cost is {soup * per_soup}")
+print("Next please!")
