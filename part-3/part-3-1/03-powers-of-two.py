c46a6e1f051e54359a1193fc8bc8cf3cab34d13c
mercurythunder <213652558+mercurythunder@users.noreply.github.com>
2025-06-10 18:44:35 +0200

    Finished Powers of base n

---
+++ part-3/part-3-1/03-powers-of-two.py
@@ -0,0 +1,5 @@
+limit = int(input("Upper limit: "))
+iteration = 1
+while iteration <= limit:
+	print(iteration)
+	iteration *= 2
---
+++ part-3/part-3-1/04-powers-of-base-n.py
@@ -0,0 +1,6 @@
+limit = int(input("Upper limit: "))
+base = int(input("Base: "))
+iteration = 1
+while iteration <= limit:
+	print(iteration)
+	iteration *= base
