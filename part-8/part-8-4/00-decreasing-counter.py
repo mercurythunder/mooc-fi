69796ff94420d7d8c87c867f00e4eef636f7c4b4
mercurythunder <213652558+mercurythunder@users.noreply.github.com>
2025-08-18 20:27:04 +0200

    Finished Decreasing counter

---
+++ part-8/part-8-4/00-decreasing-counter.py
@@ -0,0 +1,46 @@
+class DecreasingCounter:
+	def __init__(self, initial_value: int):
+		self.value = initial_value
+		self.startvalue = initial_value
+
+	def print_value(self):
+		print("value:", self.value)
+
+	# Write the rest of the methods here!
+	def decrease(self):
+		if self.value > 0:
+			self.value -= 1
+	
+	def set_to_zero(self):
+		self.value = 0
+	
+	def reset_original_value(self):
+		self.value = self.startvalue
+
+if __name__ == "__main__":
+	counter = DecreasingCounter(10)
+	counter.print_value()
+	counter.decrease()
+	counter.print_value()
+	counter.decrease()
+	counter.print_value()
+	counter = DecreasingCounter(2)
+	counter.print_value()
+	counter.decrease()
+	counter.print_value()
+	counter.decrease()
+	counter.print_value()
+	counter.decrease()
+	counter.print_value()
+	counter = DecreasingCounter(100)
+	counter.print_value()
+	counter.set_to_zero()
+	counter.print_value()
+	counter = DecreasingCounter(55)
+	counter.decrease()
+	counter.decrease()
+	counter.decrease()
+	counter.decrease()
+	counter.print_value()
+	counter.reset_original_value()
+	counter.print_value()
