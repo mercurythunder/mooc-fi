df2a9300bd0188607eceffea6fb4face49a9ab1f
mercurythunder <213652558+mercurythunder@users.noreply.github.com>
2025-08-22 17:52:56 +0200

    Finished List helper

---
+++ part-9/part-9-5/01-list-helper.py
@@ -0,0 +1,32 @@
+class ListHelper:
+	def greatest_frequency(my_list: list):
+		instances = dict()
+		for item in my_list:
+			if item in instances:
+				instances[item] += 1
+			else:
+				instances[item] = 1
+		greatest_number = 0
+		most_frequent_item = ""
+		for instance in instances:
+			if instances[instance] > greatest_number:
+				greatest_number = instances[instance]
+				most_frequent_item = instance
+		return most_frequent_item
+	
+	def doubles(my_list: list):
+		instances = dict()
+		items_doubled = 0
+		for item in my_list:
+			if item in instances:
+				instances[item] += 1
+				if instances[item] == 2:
+					items_doubled += 1
+			else:
+				instances[item] = 1
+		return items_doubled
+
+if __name__ == "__main__":
+	numbers = [1, 1, 2, 1, 3, 3, 4, 5, 5, 5, 6, 5, 5, 5]
+	print(ListHelper.greatest_frequency(numbers))
+	print(ListHelper.doubles(numbers))
